b4545dde767c6b98aa9a0444b02d8d73826a1b9a
macduyhai <dh.duyhai@gmail.com>
2017-09-07 07:20:57 +0700

    Add files via upload
    STM32_hc595_SilftOut

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int clockPin = GPIO_Pin_2;
 //Chân DS của 74HC595
 int dataPin = GPIO_Pin_3;
 //Trạng thái của LED, hay chính là byte mà ta sẽ gửi qua shiftOut
-uint16_t ledStatus=0;
+uint8_t ledStatus=0;
 /*******************************/
 
 void LED_A0_config(void)
@@ -62,15 +62,15 @@ uint64_t dwt_Micros()
 
 //////////\\\\\\\\\\\\\\\
 
- void my_shiftOut16(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint16_t val)
+ void my_shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val)
 {
-  uint16_t i;
+  uint8_t i;
   GPIO_WriteBit(GPIOA,clockPin,0);
-  for (i = 0; i < 16; i++)  {
+  for (i = 0; i < 8; i++)  {
     if (bitOrder == 0)
       GPIO_WriteBit(GPIOA,dataPin, !!(val & (1 << i)));    //khong can co !!
     else
-      GPIO_WriteBit(GPIOA,dataPin, !!(val & (1 << (15 - i))));
+      GPIO_WriteBit(GPIOA,dataPin, !!(val & (1 << (7 - i))));
 
     GPIO_WriteBit(GPIOA,clockPin,1);
     GPIO_WriteBit(GPIOA,clockPin,0);
@@ -79,19 +79,56 @@ uint64_t dwt_Micros()
   GPIO_WriteBit(GPIOA,latchPin,1);
   GPIO_WriteBit(GPIOA,latchPin,0);
 }
-
-
+void sangtuantu()
+{
+ //Sáng tuần tự
+  ledStatus = 0;//mặc định là không có đèn nào sáng hết (0 = 0b00000000)
+  for (int i = 0; i < 8; i++) {
+    ledStatus = (ledStatus << 1) | 1;//Đẩy toàn bộ các bit qua trái 1 bit và cộng bit có giá trị là 1 ở bit 0
+    GPIO_ResetBits(GPIOA,latchPin); //các đèn LED sẽ không sáng khi bạn digital LOW
+    my_shiftOut(dataPin, clockPin, 1, ledStatus);
+    GPIO_WriteBit(GPIOA,latchPin,1);//các đèn LED sẽ sáng với trạng thái vừa được cập nhập
+    GPIO_WriteBit(GPIOA,latchPin,0);  // output enable
+    delay_ms(500); // Dừng chương trình khoảng 500 mili giây để thấy các hiệu ứng của đèn LED
+  }
+}
+void batled(int i)
+ {
+  ledStatus |= (1<<i);
+ // GPIO_ResetBits(GPIOA,latchPin); //các đèn LED sẽ không sáng khi bạn digital LOW
+  my_shiftOut(dataPin, clockPin, 1, ledStatus);
+  GPIO_WriteBit(GPIOA,latchPin,1);//các đèn LED sẽ sáng với trạng thái vừa được cập nhập
+  GPIO_WriteBit(GPIOA,latchPin,0);  // output enable
+  delay_ms(500);
+ }
+ void tatled1(int i)
+ {
+  ledStatus &= ~(1<<i);
+ // GPIO_ResetBits(GPIOA,latchPin); //các đèn LED sẽ không sáng khi bạn digital LOW
+  my_shiftOut(dataPin, clockPin, 1, ledStatus);
+  GPIO_WriteBit(GPIOA,latchPin,1);//các đèn LED sẽ sáng với trạng thái vừa được cập nhập
+  GPIO_WriteBit(GPIOA,latchPin,0);  // output enable
+  delay_ms(500);
+ }
  void tatled()
  {
-  my_shiftOut16(dataPin, clockPin, 1, 0);
+  my_shiftOut(dataPin, clockPin, 1, 0);
+  GPIO_WriteBit(GPIOA,latchPin,1);//các đèn LED sẽ sáng với trạng thái vừa được cập nhập
+  GPIO_WriteBit(GPIOA,latchPin,0);
   delay_ms(100);
   ledStatus=0;
  }
  void setled(int pos, int state)
  {
-  ledStatus = (ledStatus & (~(1<<pos))) | (state << pos);
-  my_shiftOut16(dataPin, clockPin, 1, ledStatus);
+  //if(state==1) ledStatus |= (1<<pos);
+  //else  ledStatus &= ~(1<<pos);
 
+  //ledStatus = ledStatus & ~(!state<<pos) | (state << pos);
+   ledStatus = (ledStatus & (~(1<<pos))) | (state << pos);
+
+  my_shiftOut(dataPin, clockPin, 1, ledStatus);
+  GPIO_WriteBit(GPIOA,latchPin,1);//các đèn LED sẽ sáng với trạng thái vừa được cập nhập
+  GPIO_WriteBit(GPIOA,latchPin,0);  // output enable
   delay_ms(500);
  }
 /*******************************/
@@ -105,27 +142,24 @@ int main(void)
 
   while (1)
   {
-//   //////bat tat led//////
-//
-//  for(int i=0;i<16;i++)
-//  {
-//    setled(i,1);
-//    delay_ms(20);
-//  }
-//  for(int i=15;i>=0;i--)
-//  {
-//    setled(i,0);
-//    delay_ms(20);
-//  }
-
-//////////////////
-
-  my_shiftOut16(dataPin,clockPin,1,0xff00);
-  delay_ms(100);
-  my_shiftOut16(dataPin,clockPin,1,0x00ff);
-  delay_ms(100);
+  //sangtuantu();
+  for(int i=0;i<8;i++)
+  {
+    setled(i,1);
+    delay_ms(200);
+  }
+  for(int i=7;i>=0;i--)
+  {
+    setled(i,0);
+    delay_ms(200);
+  }
   //tatled();
   }
+
+  //tatled();
+  delay_ms(500);
+
+
 }// end main
 
 
